0c1840a4b833852eb6160a27cbc5fcfa040c0268
GitHub Action <action@github.com>
2024-07-04 14:21:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/anduoera/intellijplugtest/MyBundle.kt
+++ src/main/kotlin/com/github/anduoera/intellijplugtest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.anduoera.intellijplugtest
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/anduoera/intellijplugtest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/anduoera/intellijplugtest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.anduoera.intellijplugtest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.anduoera.intellijplugtest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
