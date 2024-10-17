4ef2805e0a22d46c62db06712a51ebcbe9891acc
范绍奇 <ffg.fanshq@chinatelecom.cn>
2024-10-17 21:57:16 +0800

    0.0.2 fix

--- src/main/kotlin/com/github/anduoera/intellijplug/myAnAction/ModifyErrorCodeActionService/Impl/ExException.kt
+++ src/main/kotlin/com/github/anduoera/intellijplug/myAnAction/ModifyErrorCodeActionService/Impl/ExException.kt
@@ -54,18 +54,15 @@ class ExException : ModifyErrorCodeAction {
         PsiTreeUtil.processElements(file) { element ->
             if (element.text.startsWith("exception.ErrorCode")&& element.reference?.resolve()==null&&element.text.split("+").size<2) {
                 val errorCode=element.text.split(".")[1]
-                errorCodeList.add( errorCode.replace(Regex("[^a-zA-Z].*"), ""))
+                errorCodeList.add( errorCode.replace(Regex("[^a-zA-Z0-9].*"), ""))
             }else if(element.text.startsWith("exception.ErrorMessage")&& element.reference?.resolve()==null&&element.text.split("+").size<2){
                 val errorMessage=element.text.split(".")[1]
-                errorMessageStr.add( errorMessage.replace(Regex("[^a-zA-Z].*"), ""))
+                errorMessageStr.add( errorMessage.replace(Regex("[^a-zA-Z0-9].*"), ""))
             }
             true
         }
         var toList: List<String> = errorCodeTreeSet[project.name]?.keys?.toList() ?: return
         val list = JBList(toList.reversed())
-        val listModel = DefaultListModel<String>()
-        toList.forEach { listModel.addElement(it) }
-        list.model = listModel
         val popup = JBPopupFactory.getInstance()
                 .createListPopupBuilder(list)
                 .setTitle("Choose an ErrorCode File")
@@ -76,43 +73,9 @@ class ExException : ModifyErrorCodeAction {
                         ReplaceErrorCodeUtils(chooseGoFile,project).ExException(errorCodeList,errorMessageStr)
                     }
                 }
-                .setRenderer(SimpleListCellRenderer.create<String> { label, value, _ ->
-                    label.text = value
-                })
                 .setAutoselectOnMouseMove(true)
                 .setFilterAlwaysVisible(true)
                 .createPopup()
-        // 监听键盘事件实现过滤逻辑
-        list.addKeyListener(object : KeyAdapter() {
-            private var currentFilter = ""
-
-            override fun keyTyped(e: KeyEvent) {
-                // 获取输入的字符
-                val inputChar = e.keyChar.toString()
-
-                // 如果按下的是退格键（用于删除字符），则清空 currentFilter
-                if (inputChar == KeyEvent.VK_BACK_SPACE.toChar().toString() && currentFilter.isNotEmpty()) {
-                    currentFilter = currentFilter.dropLast(1) // 移除最后一个字符
-                } else {
-                    currentFilter += inputChar // 添加新输入的字符
-                }
-
-                // 根据 currentFilter 进行模糊匹配
-                val filteredItems = if (currentFilter.isNotEmpty()) {
-                    toList.filter { item ->
-                        val regex = currentFilter.fold("", { acc, char -> "$acc.*?$char" }).toRegex(RegexOption.IGNORE_CASE)
-                        regex.containsMatchIn(item)
-                    }
-                } else {
-                    toList // 如果没有过滤条件，则返回所有项目
-                }
-
-                // 更新 listModel
-                listModel.clear()
-                filteredItems.forEach { listModel.addElement(it) }
-            }
-        })
-
         popup.showInBestPositionFor(e.dataContext)
     }
 }
